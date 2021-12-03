3c19510856fe17c58df3471ff8f6faf7d59269e6
ayalena <82506563+ayalena@users.noreply.github.com>
2021-12-03 12:18:39 +0100

    v5

---
+++ src/main/java/com/eindproject/v5/V5Application.java
@@ -0,0 +1,13 @@
+package com.eindproject.v5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eindproject/v5/V5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eindproject.v5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
